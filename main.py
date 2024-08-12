b000ebc12685bd077e8b988f792154db7b617947
blching <enternalrice@gmail.com>
2024-08-12 14:46:09 -0700

    Initalization of project

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+class Paddler:
+    def __init__(self, name, weight, side):
+        self.name = name
+        self.weight = weight
+        self.side = side
+
+p1 = Paddler("Brandon", 150, "L")
+p2 = Padder("J", 200, "R")
+p3 = Paddler("A", 100, "R")
+p4 = Paddler("C", 125, "L")
+
+all = []
+left = []
+right = []
+
+print(p1)
